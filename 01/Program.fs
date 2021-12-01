191662a777b33e9c7515b1f0c1e1f537cff47659
Radek Zikmund <radek.zikmund@bisimulations.com>
2021-12-01 08:38:40 +0100

    Initial commit

---
+++ 01/Program.fs
@@ -0,0 +1,27 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+//
+open System.IO
+
+let Part1 () =
+    let result =
+        File.ReadLines("input.txt")
+        |> Seq.map int
+        |> Seq.pairwise
+        |> Seq.filter (fun (x,y) -> x < y)
+        |> Seq.length
+
+    printf "%d" result
+
+let Part2 () =
+    let result =
+        File.ReadLines("input.txt")
+        |> Seq.map int
+        |> Seq.windowed 3
+        |> Seq.map Seq.sum
+        |> Seq.pairwise
+        |> Seq.filter (fun (x,y) -> x < y)
+        |> Seq.length
+
+    printf "%d" result
+
+Part2 ()
