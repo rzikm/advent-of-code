93b156bc39fbb32aced6517df4556d647b1df56f
Radek Zikmund <radek.zikmund@bisimulations.com>
2021-12-07 09:20:27 +0100

    Day 07

---
+++ 07/Program.fs
@@ -0,0 +1,16 @@
+﻿open System.IO;
+
+let input =
+    (File.ReadAllText("input.txt").Split(',')) |> Array.map int
+
+let run cost =
+    let min = Array.min input
+    let max = Array.max input
+
+    seq {min..max} |> Seq.map (fun x -> Array.sumBy ((-) x >> abs >> cost) input) |> Seq.min
+
+let part1 = run id
+printfn "%A" part1
+
+let part2 = run (fun x -> x * (x + 1) / 2)
+printfn "%A" part2
