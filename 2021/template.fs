f5fd9531bb242eefebc8257548955442512ad593
Radek Zikmund <radek.zikmund@bisimulations.com>
2021-12-21 12:12:54 +0100

    Day 21

--- 2021/template.fs
+++ 2021/template.fs
@@ -13,22 +13,23 @@ module Tests =
     open Xunit
     open FsUnit
 
+    let exampleInput () =
+        "" |> parse
+
     [<Fact>]
     let ``SomeTest`` () = ()
-
-    let exampleInput () = ()
 
     [<Fact>]
     let ``Example Part 1`` () =
         exampleInput () |> part1 // |> should equal
 
-    [<Fact>]
-    let ``Example Part 2`` () =
-        exampleInput () |> part2 // |> should equal
+    //[<Fact>]
+    //let ``Example Part 2`` () =
+    //    exampleInput () |> part2 // |> should equal
 
 [<EntryPoint>]
 let main _ =
-    let input = ()
+    let input = parse (File.ReadAllText "input.txt")
 
     printfn "%A" (part1 input)
     // printfn "%A" (part2 input)
