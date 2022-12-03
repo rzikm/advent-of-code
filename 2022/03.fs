bf170c762bd3436311756215b892bb490a548231
Radek Zikmund <r.zikmund.rz@gmail.com>
2022-12-03 09:34:49 +0100

    2022 Day 03

--- 2022/03.fs
+++ 2022/03.fs
@@ -4,22 +4,45 @@ open AdventOfCode
 open FSharpPlus
 open FParsec
 
-let parser = pint32
+let parser = sepEndBy (many1 (satisfy isLetter) |>> Array.ofList) spaces
 
-let solve input = 0
+let getPriority =
+    function
+    | x when isLower x -> int x - int 'a' + 1
+    | x -> int x - int 'A' + 27
 
-let solution = makeSolution parser solve solve
+let solve1 input =
+    input
+    |> List.map (fun bp ->
+        let l = Array.length bp / 2
+        let left = Array.take l bp |> Set.ofArray
+        let right = Array.skip l bp |> Set.ofArray
+        Set.intersect left right)
+    |> List.sumBy (Set.toSeq >> Seq.sumBy getPriority)
+
+let solve2 input =
+    input
+    |> List.chunkBySize 3
+    |> List.sumBy (List.map Set.ofArray >> Set.intersectMany >> Set.toSeq >> Seq.sumBy getPriority)
+
+let solution = makeSolution parser solve1 solve2
 
 module Tests =
     open Xunit
     open FsUnit.Xunit
 
-    let input = [| "" |]
+    let input =
+        [| "vJrwpWtwJgWrhcsFMMfFFhFp"
+           "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+           "PmmdzqPrVvPwwTWBwg"
+           "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
+           "ttgJtRGJQctTZtZT"
+           "CrZsJsPPZsGzwwsLwLmpwMDw" |]
 
     [<Fact>]
     let ``Example part 1`` () =
-        testPart1 solution input |> should equal 0
+        testPart1 solution input |> should equal 157
 
-// [<Fact>]
-// let ``Example part 2`` () =
-//     testPart2 solution input |> should equal 0
+    [<Fact>]
+    let ``Example part 2`` () =
+        testPart2 solution input |> should equal 70
