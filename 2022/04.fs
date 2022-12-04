6fea14a6ef7514db23a8e0580b23b255a6e2e003
Radek Zikmund <radekzikmund@microsoft.com>
2022-12-04 09:31:13 +0100

    2022 Day 04

--- 2022/04.fs
+++ 2022/04.fs
@@ -4,22 +4,38 @@ open AdventOfCode
 open FSharpPlus
 open FParsec
 
-let parser = pint32
+type Range = int32 * int32
 
-let solve input = 0
+let parser =
+    let prange = pint32 .>> skipChar '-' .>>. pint32 |>> Range
+    let ppair = prange .>> skipChar ',' .>>. prange
+    sepEndBy1 ppair (skipChar '\n')
 
-let solution = makeSolution parser solve solve
+let rangeIntersect (ll, lr) (rl, rr) = (max ll rl), (min lr rr)
+
+let fullyOverlap (l, r) =
+    let i = rangeIntersect l r
+    i = l || i = r
+
+let anyOverlap (l, r) =
+    let (il, ir) = rangeIntersect l r
+    il <= ir
+
+let solve filter input =
+    input |> List.filter filter |> List.length
+
+let solution = makeSolution parser (solve fullyOverlap) (solve anyOverlap)
 
 module Tests =
     open Xunit
     open FsUnit.Xunit
 
-    let input = [| "" |]
+    let input = [| "2-4,6-8"; "2-3,4-5"; "5-7,7-9"; "2-8,3-7"; "6-6,4-6"; "2-6,4-8" |]
 
     [<Fact>]
     let ``Example part 1`` () =
-        testPart1 solution input |> should equal 0
+        testPart1 solution input |> should equal 2
 
-// [<Fact>]
-// let ``Example part 2`` () =
-//     testPart2 solution input |> should equal 0
+    [<Fact>]
+    let ``Example part 2`` () =
+        testPart2 solution input |> should equal 4
