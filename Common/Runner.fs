70774bf61c5cab986bd3771a31cfb2639c82b4ab
Radek Zikmund <radekzikmund@microsoft.com>
2022-12-12 08:16:54 +0100

    Auto download input

--- Common/Runner.fs
+++ Common/Runner.fs
@@ -4,70 +4,104 @@ open System
 open FSharpPlus
 open AdventOfCode
 
-let parseArgs (args : string[]) =
+let parseArgs (args: string []) =
     match args with
     | [| num |] ->
         match Int32.TryParse num with
-        | true, day when day >= 1 && day <= 25 ->
-            Ok [ day ]
-        | _ ->  Error "Argument must be a single number between 1 and 25"
-    | _ ->  Ok [ 1..25 ]
+        | true, day when day >= 1 && day <= 25 -> Ok [ day ]
+        | _ -> Error "Argument must be a single number between 1 and 25"
+    | _ -> Ok [ 1..25 ]
 
-let getSolution classPrefix (day : int) =
+let getSolution classPrefix (day: int) =
     let getType typename =
         match Type.GetType typename with
-        | null -> Error (sprintf "Unable to find type %s" typename)
+        | null -> Error(sprintf "Unable to find type %s" typename)
         | t -> Ok t
 
     let className = $"{classPrefix}{day:D2}"
-    getType className >>= (fun (t : Type) ->
+
+    getType className
+    >>= (fun (t: Type) ->
         match t.GetMember "solution" |> Array.tryHead with
         | None -> Error $"Type {className} does not have member 'solution'"
         | Some me ->
             match me with
             | :? System.Reflection.PropertyInfo as fi ->
-                let solution : Solution = downcast fi.GetMethod.Invoke(null, null)
-                Ok (day, solution)
+                let solution: Solution = downcast fi.GetMethod.Invoke(null, null)
+                Ok(day, solution)
             | _ -> Error $"{className}.solution is not a property")
 
 let runWithStopwatch func arg =
-    let sw = System.Diagnostics.Stopwatch.StartNew ()
+    let sw = System.Diagnostics.Stopwatch.StartNew()
     let res = func arg
     let elapsed = sw.ElapsedMilliseconds
     (res, elapsed)
 
-let runSolution (day : int, solution : Solution) =
+let downloadInput year day =
+    let sessionFile = "../session"
+
+    if not <| IO.File.Exists sessionFile then
+        raise (new Exception("Neither the input file or the session file does not exist"))
+
+    let session = IO.File.ReadAllText sessionFile
+
+    let cookieContainer = new Net.CookieContainer()
+    cookieContainer.SetCookies(new Uri("https://adventofcode.com"), session)
+    use handler = new Net.Http.HttpClientHandler()
+
+    handler.CookieContainer <- cookieContainer
+
+    use client = new Net.Http.HttpClient(handler)
+
+    let bytes =
+        client
+            .GetByteArrayAsync(
+                $"https://adventofcode.com/{year}/day/{day}/input"
+            )
+            .Result
+
+    IO.File.WriteAllBytes($"in/{day:D2}.in", bytes)
+
+let runSolution (day: int, solution: Solution) =
     let filename = $"in/{day:D2}.in"
+
     try
+        if not <| IO.File.Exists filename then
+            downloadInput 2022 day
+
         use stream = System.IO.File.OpenRead filename
+
         let res =
-            match FParsec.CharParsers.runParserOnStream
-                (solution.parser) () filename stream (System.Text.Encoding.UTF8) with
+            match FParsec.CharParsers.runParserOnStream (solution.parser) () filename stream (System.Text.Encoding.UTF8)
+                with
             | FParsec.CharParsers.Success (input, _, _) -> Ok input
             | FParsec.CharParsers.Failure (error, _, _) -> Error error
 
-        res >>= (fun input ->
+        res
+        >>= (fun input ->
             let easy = runWithStopwatch solution.solve1 input
             let hard = runWithStopwatch solution.solve2 input
-            Ok (easy, hard))
+            Ok(easy, hard))
     with
-    | ex -> Error (ex.Message)
+    | ex -> Error(ex.Message)
 
 let run classPrefix args =
     let res =
-        args
-        |> parseArgs
-        |> Result.map (List.map (fun d -> d, getSolution classPrefix d >>= runSolution))
+        args |> parseArgs |> Result.map (List.map (fun d -> d, getSolution classPrefix d >>= runSolution))
 
     let print (day, result) =
         printfn "Day %d:" day
+
         match result with
         | Ok ((r1, t1), (r2, t2)) ->
             printfn "   Part 1: %O (%d ms)" r1 t1
             printfn "   Part 2: %O (%d ms)" r2 t2
-        | Error err ->
-            printfn "   %s" err
+        | Error err -> printfn "   %s" err
 
     match res with
-    | Ok l -> List.iter print l; 0
-    | Error s -> printfn "%s" s; 1
+    | Ok l ->
+        List.iter print l
+        0
+    | Error s ->
+        printfn "%s" s
+        1
