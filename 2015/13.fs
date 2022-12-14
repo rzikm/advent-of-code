cb93b3cf7617610157d70da78bdd529dd1efe840
Radek Zikmund <radekzikmund@microsoft.com>
2022-12-14 12:41:55 +0100

    Templates for 2015

---
+++ 2015/13.fs
@@ -0,0 +1,25 @@
+module AoC201513
+
+open AdventOfCode
+open FSharpPlus
+open FParsec
+
+let parser = pint32
+
+let solve input = 0
+
+let solution = makeSolution parser solve solve
+
+module Tests =
+    open Xunit
+    open FsUnit.Xunit
+
+    let input = [| "" |]
+
+    [<Fact>]
+    let ``Example part 1`` () =
+        testPart1 solution input |> should equal 0
+
+// [<Fact>]
+// let ``Example part 2`` () =
+//     testPart2 solution input |> should equal 0
