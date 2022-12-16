8329ce9f46c3e3d175bf3ebf8ad990f52045f7a2
Radek Zikmund <radekzikmund@microsoft.com>
2022-12-16 11:59:15 +0100

    2022 Day 16

--- 2022/16.fs
+++ 2022/16.fs
@@ -2,24 +2,111 @@ module AoC202216
 
 open AdventOfCode
 open FSharpPlus
+open FSharpPlus.Lens
+open FSharpPlus.Data
 open FParsec
 
-let parser = pint32
+let parser =
+    let pvalve = many1Chars (satisfy isAsciiUpper)
 
-let solve input = 0
+    let pline =
+        pstring "Valve " >>. pvalve
+        .>>. tuple2
+                 (pstring " has flow rate=" >>. pint32
+                  .>> (pstring "; tunnels lead to valves " <|> pstring "; tunnel leads to valve "))
+                 (sepBy1 pvalve (pstring ", "))
 
-let solution = makeSolution parser solve solve
+    sepEndBy1 pline (pchar '\n')
+
+let compressGraph start input =
+    let m = input |> Map.ofList
+
+    let fNeighbors v =
+        Map.find v m |> snd |> Seq.map (fun n -> (n, 1))
+
+    // interesting valves are the starting one and the nonzero one
+    let valves =
+        start :: (input |> List.filter (snd >> fst >> (<>) 0) |> List.map fst) |> List.distinct
+
+    valves
+    |> List.map (fun v ->
+        let rate = Map.find v m |> fst
+
+        let neighbors =
+            Graph.shortestPaths fNeighbors v (List.except [ v ] valves) |> List.map (fun (n, (_, dist)) -> (n, dist))
+
+        v, (rate, neighbors))
+    |> Map.ofList
+
+let solve timeLimit count input =
+    let compressed = compressGraph "AA" input
+
+    let rec search (time, rate, released, poss, opened) =
+        if time = timeLimit then
+            (time, rate, released, poss, opened)
+        else
+            match List.tryFindIndex (snd >> (=) 0) poss with
+            | None ->
+                // everybody is traveling/working, fast forward by the lowest time
+                let (n, dt) = poss |> List.minBy snd
+                search (time + dt, rate, released + dt * rate, (over (List.traverse << _2) (flip (-) dt) poss), opened)
+
+            | Some i ->
+                let current = poss |> List.item i |> fst
+                // somebody reached a valve and opened it
+                let (valveRate, neighbors) = Map.find current compressed
+                let rate = valveRate + rate
+
+                // state if he waits until the end of time limit
+                let ifWaited =
+                    [ search (time, rate, released, setl (List._item i << _Some) ("AA", timeLimit - time) poss, opened) ]
+
+                // state if he moves to open some valve
+                let ifMoved =
+                    neighbors
+                    |> List.choose (fun (n, dist) ->
+                        // travel + one minute to fully open the valve
+                        if time + dist + 1 < timeLimit && not <| List.contains n opened then
+                            Some(
+                                search (
+                                    time,
+                                    rate,
+                                    released,
+                                    setl (List._item i << _Some) (n, dist + 1) poss,
+                                    n :: opened
+                                )
+                            )
+                        else
+                            None)
+
+                List.concat [ ifWaited; ifMoved ] |> List.maxBy (fun (_, _, r, _, _) -> r)
+
+    let (_, _, released, _, _) = search (0, 0, 0, List.replicate count ("AA", 0), [])
+    released
+
+let solution = makeSolution parser (solve 30 1) (solve 26 2)
 
 module Tests =
     open Xunit
     open FsUnit.Xunit
 
-    let input = [| "" |]
+    let input =
+        [| "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB"
+           "Valve BB has flow rate=13; tunnels lead to valves CC, AA"
+           "Valve CC has flow rate=2; tunnels lead to valves DD, BB"
+           "Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE"
+           "Valve EE has flow rate=3; tunnels lead to valves FF, DD"
+           "Valve FF has flow rate=0; tunnels lead to valves EE, GG"
+           "Valve GG has flow rate=0; tunnels lead to valves FF, HH"
+           "Valve HH has flow rate=22; tunnel leads to valve GG"
+           "Valve II has flow rate=0; tunnels lead to valves AA, JJ"
+           "Valve JJ has flow rate=21; tunnel leads to valve II"
+           "" |]
 
     [<Fact>]
     let ``Example part 1`` () =
-        testPart1 solution input |> should equal 0
+        testPart1 solution input |> should equal 1651
 
-// [<Fact>]
-// let ``Example part 2`` () =
-//     testPart2 solution input |> should equal 0
+    [<Fact>]
+    let ``Example part 2`` () =
+        testPart2 solution input |> should equal 1707
--- Common/Graph.fs
+++ Common/Graph.fs
@@ -20,7 +20,7 @@ let inline aStar
             preds.Item n <- (nCost, v)
             fringe.Enqueue((nCost, n, v), (nCost + fHeuristic n))
 
-    startVertices |> Seq.iter (fun v -> fNeighbors v |> Seq.distinct |> Seq.iter (addNeighbor v))
+    startVertices |> Seq.iter (fun v -> fNeighbors v |> Seq.iter (addNeighbor v))
 
     let rec finalPath v tail =
         if Set.contains v starts then
@@ -45,3 +45,50 @@ let inline aStar
             doSearch ()
 
     doSearch ()
+
+let shortestPaths
+    (fNeighbors: 'vertex -> ('vertex * ^cost) seq)
+    (start: 'vertex)
+    (ends: 'vertex list)
+    =
+
+    let preds = Dictionary<'vertex, 'cost * 'vertex>()
+    let fringe = PriorityQueue<'cost * 'vertex * 'vertex, 'cost>()
+
+    let addNeighbor v (n, nCost) =
+        match preds.TryGetValue n with
+        | true, (nPrevCost, _) when nPrevCost <= nCost -> () // already have a better path to n
+        | _ -> // found a better path to n via v
+            fringe.Enqueue((nCost, n, v), nCost)
+
+    fNeighbors start |> Seq.iter (addNeighbor start)
+
+    let rec finalPath v tail =
+        if v = start then
+            v :: tail
+        else
+            match preds.TryGetValue v with
+            | false, _ -> v :: tail
+            | true, (_, x) -> finalPath x (v :: tail)
+
+    let rec doSearch ends acc =
+        if ends = [] then acc
+        else
+            let (vCost, v, from) = fringe.Dequeue()
+
+            if not <| preds.ContainsKey(v) then
+                // found shortest path to 'v' via 'from'
+                preds.Item v <- (vCost, from)
+
+                for (n, nCost) in fNeighbors v do
+                    if v <> start then
+                        addNeighbor v (n, vCost + nCost)
+
+                if List.contains v ends then
+                    doSearch (List.except [v] ends) ((v, (finalPath v [], vCost)):: acc)
+                else
+                    doSearch ends acc
+            else
+                doSearch ends acc
+
+    doSearch ends []
