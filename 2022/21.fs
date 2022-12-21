9e23ea2cc54e237c96e81399cf177fed663aa95f
Radek Zikmund <radekzikmund@microsoft.com>
2022-12-21 13:54:43 +0100

    Some small refactoring

--- 2022/21.fs
+++ 2022/21.fs
@@ -2,28 +2,31 @@ module AoC202221
 
 open AdventOfCode
 open FSharpPlus
+open FSharpPlus.Lens
 open FParsec
+
+type Operator =
+    | Plus
+    | Minus
+    | Times
+    | Divide
 
 type Expression =
     | Number of int64
-    | Sum of string * string
-    | Diff of string * string
-    | Product of string * string
-    | Ratio of string * string
+    | Operator of string * Operator * string
 
 let parser =
     let pmonkey = many1Chars (satisfy isLetter)
 
+    let pOperator =
+        choice [ charReturn '+' Plus
+                 charReturn '-' Minus
+                 charReturn '*' Times
+                 charReturn '/' Divide ]
+
     let pExpr =
         choice [ pint64 |>> Number
-                 pmonkey .>> pchar ' ' .>>. anyOf [ '+'; '-'; '*'; '/' ] .>> pchar ' ' .>>. pmonkey
-                 |>> (fun ((l, op), r) ->
-                     match op with
-                     | '+' -> Sum(l, r)
-                     | '-' -> Diff(l, r)
-                     | '*' -> Product(l, r)
-                     | '/' -> Ratio(l, r)
-                     | _ -> failwith "unreachable") ]
+                 tuple3 (pmonkey) (pchar ' ' >>. pOperator .>> pchar ' ') pmonkey |>> Operator ]
 
     sepEndBy1 (pmonkey .>> pstring ": " .>>. pExpr) (pchar '\n')
 
@@ -31,74 +34,74 @@ let solve1 input =
     let exprs = Map.ofList input
 
     let eval =
-        Utils.memoizerec (fun frec ex ->
+        Utils.memoizerec (fun eval ex ->
             match Map.find ex exprs with
             | Number i -> i
-            | Sum (l, r) -> frec l + frec r
-            | Diff (l, r) -> frec l - frec r
-            | Product (l, r) -> frec l * frec r
-            | Ratio (l, r) -> frec l / frec r)
+            | Operator (l, op, r) ->
+                match op with
+                | Plus -> eval l + eval r
+                | Minus -> eval l - eval r
+                | Times -> eval l * eval r
+                | Divide -> eval l / eval r)
 
     eval "root"
 
+// prism for targeting the Operator case of Expression
+let inline _Operator e =
+    (prism Operator (fun e ->
+        match e with
+        | Operator (l, op, r) -> Result.Ok((l, op, r))
+        | _ -> Result.Error(e)))
+        e
+
 let solve2 input =
-    let exprs = Map.ofList input
+    let exprs =
+        Map.ofList input
+        // update root to minus expression, we solve for 0
+        |> setl (Map._item "root" << _Some << _Operator << _2) Minus
 
-    let (left, right) =
-        match Map.find "root" exprs with
-        | Sum (l, r) -> l, r
-        | Diff (l, r) -> l, r
-        | Product (l, r) -> l, r
-        | Ratio (l, r) -> l, r
-        | _ -> failwith "unreachable"
-
+    // eval the expression to constant or None if contains the free variable "humn"
     let eval =
-        Utils.memoizerec (fun frec ex ->
+        Utils.memoizerec (fun eval ex ->
             if ex = "humn" then
                 None
             else
                 match Map.find ex exprs with
                 | Number i -> Some i
-                | Sum (l, r) -> Option.map2 (+) (frec l) (frec r)
-                | Diff (l, r) -> Option.map2 (-) (frec l) (frec r)
-                | Product (l, r) -> Option.map2 (*) (frec l) (frec r)
-                | Ratio (l, r) -> Option.map2 (/) (frec l) (frec r))
+                | Operator (l, op, r) ->
+                    match op with
+                    | Plus -> Option.map2 (+) (eval l) (eval r)
+                    | Minus -> Option.map2 (-) (eval l) (eval r)
+                    | Times -> Option.map2 (*) (eval l) (eval r)
+                    | Divide -> Option.map2 (/) (eval l) (eval r))
 
+    // Assume the free variable appears only on a single path in the expression tree, then we can
+    // go backwards from tree and solve for "humn"
     let rec reverse targetValue expr =
         if expr = "humn" then
             targetValue
         else
             match Map.find expr exprs with
-            | Sum (l, r) ->
-                // targetValue = l + r
+            | Number _ -> failwith "Expression is a constant"
+            | Operator (l, op, r) ->
+                // targetValue = l `op` r =>
                 match (eval l, eval r) with
-                | Some l, None -> reverse (targetValue - l) r
-                | None, Some r -> reverse (targetValue - r) l
-                | _ -> failwith "Unexpected"
-            | Diff (l, r) ->
-                // targetValue = l - r
-                match (eval l, eval r) with
-                | Some l, None -> reverse (l - targetValue) r
-                | None, Some r -> reverse (targetValue + r) l
-                | _ -> failwith "Unexpected"
-            | Product (l, r) ->
-                // targetValue = l * r
-                match (eval l, eval r) with
-                | Some l, None -> reverse (targetValue / l) r
-                | None, Some r -> reverse (targetValue / r) l
-                | _ -> failwith "Unexpected"
-            | Ratio (l, r) ->
-                // targetValue = l / r
-                match (eval l, eval r) with
-                | Some l, None -> reverse (l / targetValue) r
-                | None, Some r -> reverse (targetValue * r) l
-                | _ -> failwith "Unexpected"
-            | _ -> failwith "unreachable"
+                | Some l, None ->
+                    match op with
+                    | Plus -> reverse (targetValue - l) r
+                    | Minus -> reverse (l - targetValue) r
+                    | Times -> reverse (targetValue / l) r
+                    | Divide -> reverse (l / targetValue) r
+                | None, Some r ->
+                    match op with
+                    | Plus -> reverse (targetValue - r) l
+                    | Minus -> reverse (targetValue + r) l
+                    | Times -> reverse (targetValue / r) l
+                    | Divide -> reverse (targetValue * r) l
+                | _ -> failwith "Expected exactly one subexpression to eval to a constant"
 
-    match (eval left, eval right) with
-    | Some i, None -> reverse i right
-    | None, Some i -> reverse i left
-    | _ -> failwith "Unexpected"
+    reverse 0 "root"
+
 
 let solution = makeSolution parser solve1 solve2
 
