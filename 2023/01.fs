2e1b53bf4b15024798c21e4af3369b89c5bb3298
Radek Zikmund <r.zikmund.rz@gmail.com>
2023-12-01 09:23:39 +0100

    2023 day 1

--- 2023/01.fs
+++ 2023/01.fs
@@ -4,22 +4,53 @@ open AdventOfCode
 open FSharpPlus
 open FParsec
 
-let parser = pint32
+let parser = sepEndBy1 (manyChars <| satisfy (fun c -> isLetter c || isDigit c)) (pchar '\n')
 
-let solve input = 0
+let digits = [ 1..9 ] |> List.map (fun d -> string d, d)
+let digitsAsWords = [ "one", 1; "two", 2; "three", 3; "four", 4; "five", 5; "six", 6; "seven", 7; "eight", 8; "nine", 9 ]
 
-let solution = makeSolution parser solve solve
+let solve digits input =
+    let idxes find = 
+        digits
+        |> List.choose (fun (w, d) ->
+            let i = find w
+            if (i >= 0) then Some((d, i)) else None)
+
+    let f (s : string) =
+        let first = idxes (fun (w : string) -> s.IndexOf(w)) |> List.minBy snd |> fst
+        let last = idxes (fun (w : string) -> s.LastIndexOf(w)) |> List.maxBy snd |> fst
+
+        10 * first + last
+
+    input |> List.map f |> List.sum
+
+let solution = makeSolution parser (solve digits) (solve <| List.append digits digitsAsWords)
 
 module Tests =
     open Xunit
     open FsUnit.Xunit
 
-    let input = [| "" |]
+    let input = [| 
+        "1abc2"
+        "pqr3stu8vwx"
+        "a1b2c3d4e5f"
+        "treb7uchet"
+     |]
 
     [<Fact>]
     let ``Example part 1`` () =
-        testPart1 solution input |> should equal 0
+        testPart1 solution input |> should equal 142
 
-// [<Fact>]
-// let ``Example part 2`` () =
-//     testPart2 solution input |> should equal 0
+    let input2 = [| 
+        "two1nine"
+        "eightwothree"
+        "abcone2threexyz"
+        "xtwone3four"
+        "4nineeightseven2"
+        "zoneight234"
+        "7pqrstsixteen"
+     |]
+
+    [<Fact>]
+    let ``Example part 2`` () =
+        testPart2 solution input2 |> should equal 281
