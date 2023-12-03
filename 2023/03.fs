f8a2e98238f0dd1d1e8827024c4599813b15ee83
Radek Zikmund <r.zikmund.rz@gmail.com>
2023-12-03 11:45:10 +0100

    2023 Day 03

--- 2023/03.fs
+++ 2023/03.fs
@@ -4,24 +4,75 @@ open AdventOfCode
 open FSharpPlus
 open FParsec
 
-let parser = pint32
+type Input =
+    { numbers: (int64 * (int64 * int64 * int64)) list
+      symbols: (char * (int64 * int64)) list }
 
-let solve1 input = 0
+let parser =
+    let emptyInput = { numbers = []; symbols = [] }
 
-let solve2 input = 0
+    let number =
+        lookAhead (satisfy isDigit)
+        >>. (pipe3 getPosition pint64 getPosition
+             <| fun pos n pos2 -> { numbers = [ (n, (pos.Line, pos.Column, pos2.Column - 1L)) ]; symbols = [] })
 
-let solution = makeSolution parser solve1 solve2
+    let space = skipChar '.' >>% emptyInput
+    let newline = skipChar '\n' >>% emptyInput
+
+    let symbol =
+        getPosition .>>. satisfy (not << System.Char.IsWhiteSpace)
+        |>> fun (pos, c) -> { numbers = []; symbols = [ (c, (pos.Line, pos.Column)) ] }
+
+    let reduce (a: Input) (b: Input) =
+        { numbers = (a.numbers @ b.numbers); symbols = (a.symbols @ b.symbols) }
+
+    many1 (
+        choice [ number
+                 space
+                 symbol
+                 newline ]
+    )
+    |>> List.reduce reduce
+
+let isAdjacentToSymbol (n, (r, c, c2)) (c, (r', c')) =
+    abs (r - r') <= 1L && (c - 1L <= c' && c' <= c2 + 1L)
+
+let adjacentNumbers numbers c =
+    numbers |> List.filter (fun n -> isAdjacentToSymbol n c)
+
+let solve1 (input: Input) =
+    input.numbers |> List.filter (fun n -> List.exists (isAdjacentToSymbol n) input.symbols) |> List.sumBy fst
+
+let solve2 (input: Input) =
+
+    input.symbols
+    |> List.filter (fst >> (=) '*')
+    |> List.map (adjacentNumbers input.numbers)
+    |> List.filter (List.length >> (=) 2)
+    |> List.sumBy (List.map fst >> List.reduce (*))
+
+let solution = makeSolution () parser solve1 solve2
 
 module Tests =
     open Xunit
     open FsUnit.Xunit
 
-    let input = [| "" |]
+    let input =
+        [| "467..114.."
+           "...*......"
+           "..35..633."
+           "......#..."
+           "617*......"
+           ".....+.58."
+           "..592....."
+           "......755."
+           "...$.*...."
+           ".664.598.." |]
 
-// [<Fact>]
-// let ``Example part 1`` () =
-//     testPart1 solution input |> should equal 0
+    [<Fact>]
+    let ``Example part 1`` () =
+        testPart1 solution input |> should equal 4361L
 
-// [<Fact>]
-// let ``Example part 2`` () =
-//     testPart2 solution input |> should equal 0
+    [<Fact>]
+    let ``Example part 2`` () =
+        testPart2 solution input |> should equal 467835L
