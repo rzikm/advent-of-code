7fa88cb8b544b9dc38aef9242d3b7a12d0b82c6c
Radek Zikmund <r.zikmund.rz@gmail.com>
2023-12-09 09:34:12 +0100

    2023 Day 09

--- 2023/09.fs
+++ 2023/09.fs
@@ -4,11 +4,21 @@ open AdventOfCode
 open FSharpPlus
 open FParsec
 
-let parser = pint32
+let parser =
+    let seq = sepBy1 pint32 (skipChar ' ')
+    sepEndBy1 seq (skipChar '\n')
 
-let solve1 input = 0
+let computeNextInSeq seq =
+    let unfolder seq =
+        List.tryFind ((<>) 0) seq // continue until all zeros
+        |> Option.map (fun _ -> List.last seq, List.pairwise seq |> List.map (fun (a, b) -> b - a))
 
-let solve2 input = 0
+    List.unfold unfolder seq |> List.rev |> List.scan (+) 0 |> List.last
+
+let solve1 input =
+    input |> List.map computeNextInSeq |> List.sum
+
+let solve2 input = input |> List.map List.rev |> solve1
 
 let solution = makeSolution () parser solve1 solve2
 
@@ -16,12 +26,12 @@ module Tests =
     open Xunit
     open FsUnit.Xunit
 
-    let input = [| "" |]
+    let input = [| "0 3 6 9 12 15"; "1 3 6 10 15 21"; "10 13 16 21 30 45"; "" |]
 
-// [<Fact>]
-// let ``Example part 1`` () =
-//     testPart1 solution input |> should equal 0
+    [<Fact>]
+    let ``Example part 1`` () =
+        testPart1 solution input |> should equal 114
 
-// [<Fact>]
-// let ``Example part 2`` () =
-//     testPart2 solution input |> should equal 0
+    [<Fact>]
+    let ``Example part 2`` () =
+        testPart2 solution input |> should equal 2
