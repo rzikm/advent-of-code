acd1028e9382fec31e630e0b98e3eeb5db6d188f
Radek Zikmund <r.zikmund.rz@gmail.com>
2023-12-17 22:56:11 +0100

    2019 Day 03

--- 2019/02.fs
+++ 2019/02.fs
@@ -4,11 +4,38 @@ open AdventOfCode
 open FSharpPlus
 open FParsec
 
-let parser = pint32
+let parser = sepBy1 pint64 (pchar ',') |>> Array.ofList
 
-let solve1 input = 0
+let rec run input i =
+    let opCode = Array.item i input
+    let a = Array.item (i + 1) input
+    let b = Array.item (i + 2) input
+    let c = Array.item (i + 3) input
 
-let solve2 input = 0
+    match opCode with
+    | 1L ->
+        input.[int32 c] <- input.[int32 a] + input.[int32 b]
+        run input (i + 4)
+    | 2L ->
+        input.[int32 c] <- input.[int32 a] * input.[int32 b]
+        run input (i + 4)
+    | 99L -> ()
+    | _ -> failwithf "Unknown opcode %d" opCode
+
+let runWith (input: int64 array) noun verb =
+    input.[1] <- noun
+    input.[2] <- verb
+    run input 0
+    input |> Array.head
+
+let solve1 input = runWith input 12L 2L
+
+let solve2 input =
+    List.allPairs [ 0L .. 99L ] [
+        0L .. 99L
+    ]
+    |> Seq.find (fun (noun, verb) -> runWith (input |> Array.copy) noun verb = 19690720L)
+    |> (fun (noun, verb) -> 100L * noun + verb)
 
 let solution = makeSolution () parser solve1 solve2
 
@@ -16,12 +43,10 @@ module Tests =
     open Xunit
     open FsUnit.Xunit
 
-    let input = [| "" |]
+    let input = [| "1,9,10,3,2,3,11,0,99,30,40,50" |]
 
     [<Fact>]
     let ``Example part 1`` () =
-        testPart1 solution input |> should equal 0
-
-//     [<Fact>]
-//     let ``Example part 2`` () =
-//         testPart2 solution input |> should equal 0
+        let input = parseTestInput parser input
+        run input 0
+        input.[0] |> should equal 3500L
