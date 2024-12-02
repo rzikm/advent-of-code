33c4041e527a09c73a78031021a4a3f3d252a9aa
Radek Zikmund <r.zikmund.rz@gmail.com>
2024-12-02 12:47:34 +0100

    2024 Day 1

--- 2024/01.fs
+++ 2024/01.fs
@@ -4,11 +4,16 @@ open AdventOfCode
 open FSharpPlus
 open FParsec
 
-let parser = spaces
+let parser = ParseUtils.lines (pint32 .>> spaces .>>. pint32)
 
-let solve1 input = 0
+let solve1 input =
+    input |> List.unzip |> Tuple2.map List.sort |> uncurry List.zip |> List.sumBy (uncurry (-) >> abs)
 
-let solve2 input = 0
+let solve2 input =
+    let (left, right) = input |> List.unzip
+    let occ = right |> List.countBy id |> Map.ofList
+
+    left |> List.sumBy (fun n -> Map.tryFind n occ |> Option.defaultValue 0 |> (fun x -> x * n))
 
 let solution = makeSolution () parser solve1 solve2
 
@@ -16,12 +21,12 @@ module Tests =
     open Xunit
     open FsUnit.Xunit
 
-    let input = [| "" |]
+    let input = [| "3   4"; "4   3"; "2   5"; "1   3"; "3   9"; "3   3" |]
 
     [<Fact>]
     let ``Example part 1`` () =
-        testPart1 solution input |> should equal 0
+        testPart1 solution input |> should equal 11
 
-//     [<Fact>]
-//     let ``Example part 2`` () =
-//         testPart2 solution input |> should equal 0
+    [<Fact>]
+    let ``Example part 2`` () =
+        testPart2 solution input |> should equal 31
