b148ed3ac26ee03d976ce5f20f9e3ef2e5fac044
Radek Zikmund <r.zikmund.rz@gmail.com>
2024-12-03 12:05:38 +0100

    2024 Day 03

--- 2024/03.fs
+++ 2024/03.fs
@@ -1,14 +1,36 @@
 module AoC202403
 
 open AdventOfCode
-open FSharpPlus
 open FParsec
 
-let parser = spaces
+type Instruction =
+    | Mul of int * int
+    | Do
+    | Dont
 
-let solve1 input = 0
+let parser =
+    let pmul =
+        attempt (pstring "mul(" >>. pint32 .>> pchar ',' .>>. pint32 .>> pchar ')' |>> Mul)
 
-let solve2 input = 0
+    let pdo = stringReturn "do()" Do
+    let pdont = stringReturn "don't()" Dont
+
+    many1 ((choice [ pmul; pdo; pdont ] |>> Some) <|> (anyChar >>% None)) |>> List.choose id
+
+let solve1 =
+    List.sumBy (function
+        | Mul (a, b) -> a * b
+        | _ -> 0)
+
+let solve2 =
+    List.fold
+        (fun (acc, s) m ->
+            match m with
+            | Mul (a, b) -> (acc + (if s then a * b else 0), s)
+            | Do -> (acc, true)
+            | Dont -> (acc, false))
+        (0, true)
+    >> fst
 
 let solution = makeSolution () parser solve1 solve2
 
@@ -16,12 +38,16 @@ module Tests =
     open Xunit
     open FsUnit.Xunit
 
-    let input = [| "" |]
+    let input1 =
+        [| "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))" |]
+
+    let input2 =
+        [| "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))" |]
 
     [<Fact>]
     let ``Example part 1`` () =
-        testPart1 solution input |> should equal 0
+        testPart1 solution input1 |> should equal 161
 
-//     [<Fact>]
-//     let ``Example part 2`` () =
-//         testPart2 solution input |> should equal 0
+    [<Fact>]
+    let ``Example part 2`` () =
+        testPart2 solution input2 |> should equal 48
