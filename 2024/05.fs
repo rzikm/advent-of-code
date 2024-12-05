962aa801f386959491adb7661b9ac68fdb761cc0
Radek Zikmund <r.zikmund.rz@gmail.com>
2024-12-05 14:49:09 +0100

    2024 Day 05

--- 2024/05.fs
+++ 2024/05.fs
@@ -4,11 +4,34 @@ open AdventOfCode
 open FSharpPlus
 open FParsec
 
-let parser = spaces
+let parser =
+    let prules = ParseUtils.lines (pint32 .>> pchar '|' .>>. pint32)
+    let pageLists = ParseUtils.lines (sepBy1 pint32 (pchar ','))
 
-let solve1 input = 0
+    prules .>> pchar '\n' .>>. pageLists
 
-let solve2 input = 0
+let orderByRules rules =
+    let ruleSet = Set.ofList rules
+
+    List.sortWith (fun a b ->
+        if Set.contains (a, b) ruleSet then -1
+        else if Set.contains (b, a) ruleSet then 1
+        else 0)
+
+let getScore (pages: 'a list) = pages.[(List.length pages / 2)]
+
+let solve1 (rules, lists) =
+    let sort = orderByRules rules
+    // lists |> List.filter (isCorrectlyOrdered rules) |> List.sumBy getScore
+    lists |> List.sumBy (fun pages -> if (sort pages) = pages then getScore pages else 0)
+
+let solve2 (rules, lists) =
+    let sort = orderByRules rules
+
+    lists
+    |> List.sumBy (fun pages ->
+        let sorted = sort pages
+        if sorted = pages then 0 else getScore sorted)
 
 let solution = makeSolution () parser solve1 solve2
 
@@ -16,12 +39,40 @@ module Tests =
     open Xunit
     open FsUnit.Xunit
 
-    let input = [| "" |]
+    let input =
+        [| "47|53"
+           "97|13"
+           "97|61"
+           "97|47"
+           "75|29"
+           "61|13"
+           "75|53"
+           "29|13"
+           "97|29"
+           "53|29"
+           "61|53"
+           "97|53"
+           "61|29"
+           "47|13"
+           "75|47"
+           "97|75"
+           "47|61"
+           "75|61"
+           "47|29"
+           "75|13"
+           "53|13"
+           ""
+           "75,47,61,53,29"
+           "97,61,53,29,13"
+           "75,29,13"
+           "75,97,47,61,53"
+           "61,13,29"
+           "97,13,75,29,47" |]
 
     [<Fact>]
     let ``Example part 1`` () =
-        testPart1 solution input |> should equal 0
+        testPart1 solution input |> should equal 143
 
-//     [<Fact>]
-//     let ``Example part 2`` () =
-//         testPart2 solution input |> should equal 0
+    [<Fact>]
+    let ``Example part 2`` () =
+        testPart2 solution input |> should equal 123
