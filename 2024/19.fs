5be4b84570c0b7e6048c961a6521cda817f77dbb
Radek Zikmund <r.zikmund.rz@gmail.com>
2024-12-19 11:08:13 +0100

    2024 Day 19

--- 2024/19.fs
+++ 2024/19.fs
@@ -4,11 +4,27 @@ open AdventOfCode
 open FSharpPlus
 open FParsec
 
-let parser = spaces
+let parser =
+    let word = many1Chars <| satisfy isAsciiLetter
+    let available = sepBy1 word (skipString ", ")
+    available .>> skipNewline .>> skipNewline .>>. ParseUtils.lines word
 
-let solve1 input = 0
+let makeCountCompositions words =
+    Utils.memoizerec (fun frec w ->
+        if w = "" then
+            1L
+        else
+            words
+            |> Seq.filter (fun prefix -> String.startsWith prefix w)
+            |> Seq.sumBy (fun prefix -> frec (String.drop (String.length prefix) w)))
 
-let solve2 input = 0
+let solve1 (available, toSolve) =
+    let getComposition = makeCountCompositions <| Set.ofList available
+    toSolve |> List.count (getComposition >> flip (>) 0)
+
+let solve2 (available, toSolve) =
+    let getComposition = makeCountCompositions <| Set.ofList available
+    toSolve |> List.sumBy getComposition
 
 let solution = makeSolution () parser solve1 solve2
 
@@ -16,12 +32,22 @@ module Tests =
     open Xunit
     open FsUnit.Xunit
 
-    let input = [| "" |]
+    let input =
+        [| "r, wr, b, g, bwu, rb, gb, br"
+           ""
+           "brwrr"
+           "bggr"
+           "gbbr"
+           "rrbgbr"
+           "ubwu"
+           "bwurrg"
+           "brgr"
+           "bbrgwb" |]
 
     [<Fact>]
     let ``Example part 1`` () =
-        testPart1 solution input |> should equal 0
+        testPart1 solution input |> should equal 6
 
-//     [<Fact>]
-//     let ``Example part 2`` () =
-//         testPart2 solution input |> should equal 0
+    [<Fact>]
+    let ``Example part 2`` () =
+        testPart2 solution input |> should equal 16L
