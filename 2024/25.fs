15226047b19f3dc2a2a25f31dae4d7c3fe11532c
Radek Zikmund <r.zikmund.rz@gmail.com>
2024-12-25 18:58:09 +0100

    2024 Day 25

--- 2024/25.fs
+++ 2024/25.fs
@@ -4,11 +4,36 @@ open AdventOfCode
 open FSharpPlus
 open FParsec
 
-let parser = spaces
+let parser =
+    let row = parray 5 (anyOf "#.")
 
-let solve1 input = 0
+    let plock =
+        stringReturn "#####" true .>> skipNewline .>>. parray 6 (row .>> spaces)
+        |>> Tuple2.mapItem2 (fun r ->
+            Array.transpose r
+            |> Array.map (String.ofArray >> String.takeWhile ((=) '#') >> String.length)
+            |> List.ofArray)
 
-let solve2 input = 0
+    let pkey =
+        stringReturn "....." false .>> skipNewline .>>. parray 6 (row .>> spaces)
+        |>> Tuple2.mapItem2 (fun r ->
+            Array.transpose r
+            |> Array.map (String.ofArray >> String.rev >> String.takeWhile ((=) '#') >> String.length)
+            |> List.ofArray)
+
+    sepEndBy1 ((attempt plock) <|> (attempt pkey)) spaces
+
+let solve1 input =
+    let keys, locks = input |> List.partition fst
+
+    let keys = keys |> List.map snd
+    let locks = locks |> List.map snd
+
+    keys
+    |> List.sumBy (fun key ->
+        locks |> List.count (fun lock -> List.zip key lock |> List.forall (fun (k, l) -> k + l <= 6)))
+
+let solve2 _ = "*"
 
 let solution = makeSolution () parser solve1 solve2
 
@@ -16,11 +41,50 @@ module Tests =
     open Xunit
     open FsUnit.Xunit
 
-    let input = [| "" |]
+    let input =
+        [| "#####"
+           ".####"
+           ".####"
+           ".####"
+           ".#.#."
+           ".#..."
+           "....."
+           ""
+           "#####"
+           "##.##"
+           ".#.##"
+           "...##"
+           "...#."
+           "...#."
+           "....."
+           ""
+           "....."
+           "#...."
+           "#...."
+           "#...#"
+           "#.#.#"
+           "#.###"
+           "#####"
+           ""
+           "....."
+           "....."
+           "#.#.."
+           "###.."
+           "###.#"
+           "###.#"
+           "#####"
+           ""
+           "....."
+           "....."
+           "....."
+           "#...."
+           "#.#.."
+           "#.#.#"
+           "#####" |]
 
     [<Fact>]
     let ``Example part 1`` () =
-        testPart1 solution input |> should equal 0
+        testPart1 solution input |> should equal 3
 
 //     [<Fact>]
 //     let ``Example part 2`` () =
