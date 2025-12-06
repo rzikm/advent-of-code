04cb5d97fdb74a8c8cf8f18e9f0c8703a07682c8
Radek Zikmund <r.zikmund.rz@gmail.com>
2025-12-06 11:11:27 +0100

    2025 Day 6

--- 2025/06.fs
+++ 2025/06.fs
@@ -4,11 +4,36 @@ open AdventOfCode
 open FSharpPlus
 open FParsec
 
-let parser = spaces
+let parser =
+    let grid = ParseUtils.grid (digit <|> pchar ' ')
+    let ops = ParseUtils.skipSpaces >>. ParseUtils.sepBySpaces (anyOf "*+")
+    grid .>>. ops
 
-let solve1 input = 0
+let foldWithOp c list =
+    match c with
+    | '*' -> List.fold (*) 1L list
+    | '+' -> List.fold (+) 0L list
+    | _ -> failwith "unexpected operator"
 
-let solve2 input = 0
+let solve1 (grid, ops) =
+    let grid =
+        grid
+        |> Array.map (Array.split [ [| ' ' |] ] >> Seq.choose (String.ofArray >> tryParse<int64>) >> Seq.toList)
+        |> List.ofArray
+
+    List.transpose grid |> List.zip ops |> List.sumBy (fun (op, col) -> foldWithOp op col)
+
+let solve2 (grid, ops) =
+    let grid =
+        grid
+        |> Array.transpose
+        |> Array.map (Array.split [ [| ' ' |] ] >> Seq.choose (String.ofArray >> tryParse<int64>) >> Seq.toList)
+        |> List.ofArray
+        |> List.split [ [ [] ] ]
+        |> List.ofSeq
+        |> List.map (List.map List.exactlyOne)
+
+    grid |> Seq.zip ops |> Seq.sumBy (fun (op, col) -> foldWithOp op col)
 
 let solution = makeSolution () parser solve1 solve2
 
@@ -16,12 +41,19 @@ module Tests =
     open Xunit
     open FsUnit.Xunit
 
-    let input = [||]
+    let input =
+        [| "123 328  51 64 "; " 45 64  387 23 "; "  6 98  215 314"; "*   +   *   +  " |]
 
     [<Fact>]
     let ``Example part 1`` () =
-        testPart1 solution input |> should equal 0
+        testPart1 solution input |> should equal 4277556L
 
-//     [<Fact>]
-//     let ``Example part 2`` () =
-//         testPart2 solution input |> should equal 0
+    [<Fact>]
+    let ``Split test`` () =
+        let data = [ [ 1 ]; [ 2 ]; []; [ 3 ]; [ 4 ] ]
+        let result = data |> List.split [ [ [] ] ] |> List.ofSeq
+        result |> should equal [ [ [ 1 ]; [ 2 ] ]; [ [ 3 ]; [ 4 ] ] ]
+
+    [<Fact>]
+    let ``Example part 2`` () =
+        testPart2 solution input |> should equal 3263827L
--- Common/ParseUtils.fs
+++ Common/ParseUtils.fs
@@ -2,7 +2,8 @@ module ParseUtils
 
 open FParsec
 
-let lines line = sepEndBy1 line (skipChar '\n')
+let lines line =
+    sepEndBy1 (attempt line) (skipChar '\n')
 
 let grid cell =
     let row = many1 cell |>> Array.ofList
@@ -10,3 +11,7 @@ let grid cell =
 
 let notWhitespace<'a> : Parser<char, 'a> =
     satisfy (fun c -> not <| System.Char.IsWhiteSpace c)
+
+let skipSpaces: Parser<unit, unit> = skipMany (pchar ' ')
+
+let sepBySpaces p = sepEndBy p skipSpaces
