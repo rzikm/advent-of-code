117ef7a1e579333e97c709f44dcbaa022c1fe107
Radek Zikmund <r.zikmund.rz@gmail.com>
2025-12-07 10:20:02 +0100

    2025 Day 7

--- Common/ParseUtils.fs
+++ Common/ParseUtils.fs
@@ -5,9 +5,13 @@ open FParsec
 let lines line =
     sepEndBy1 (attempt line) (skipChar '\n')
 
+
 let grid cell =
     let row = many1 cell |>> Array.ofList
     lines row |>> Array.ofList
+
+let gridOf (cellList: (char * 'Cell) list) =
+    choice (cellList |> List.map (fun (c, cell) -> charReturn c cell)) |> grid
 
 let notWhitespace<'a> : Parser<char, 'a> =
     satisfy (fun c -> not <| System.Char.IsWhiteSpace c)
